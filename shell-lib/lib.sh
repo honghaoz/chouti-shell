76f5f872c08069dce08790b2acb0af95d0127e30
Honghao Zhang <m@honghao.dev>
2023-11-08 15:32:47 -0800

    add swift.sh

--- shell-lib/lib.sh
+++ shell-lib/lib.sh
@@ -5,3 +5,6 @@ source "/usr/local/lib/shell-lib/colors.sh"
 
 #shellcheck disable=SC1091
 source "/usr/local/lib/shell-lib/helpers.sh"
+
+#shellcheck disable=SC1091
+source "/usr/local/lib/shell-lib/swift.sh"
