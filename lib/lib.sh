1258737a26412f9941fe4df8758d39a6da1d79bf
Honghao Zhang <m@honghao.dev>
2023-12-18 20:22:36 -0800

    [xcode] update xcode project-marketing-versions to print detailed versions

--- lib/lib.sh
+++ lib/lib.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# avoid overwriting the original SCRIPT_DIR
+original_script_path="$SCRIPT_DIR"
+
 if [[ -n "$BASH_VERSION" ]]; then
-  SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+  SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 elif [[ -n "$ZSH_VERSION" ]]; then
   # https://stackoverflow.com/questions/18810483/what-is-the-zsh-equivalent-of-a-bash-script-getting-the-scripts-directory
   SCRIPT_DIR=${0:a:h}
@@ -15,3 +18,6 @@ source "${SCRIPT_DIR}/colors.sh"
 
 # shellcheck disable=SC1091
 source "${SCRIPT_DIR}/utilities.sh"
+
+SCRIPT_DIR="$original_script_path"
+unset original_script_path
